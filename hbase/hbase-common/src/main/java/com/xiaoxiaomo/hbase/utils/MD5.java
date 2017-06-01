5c1af50c8cb7f2b3c95f4028136a38298c458c67
jasonTangxd <jason.tangxd@gmail.com>
2017-06-01 13:49:12 +0800

    md5

---
+++ hbase/hbase-common/src/main/java/com/xiaoxiaomo/hbase/utils/MD5.java
@@ -0,0 +1,38 @@
+package com.xiaoxiaomo.hbase.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * Created by xiaoxiaomo on 2017/6/1.
+ */
+public class MD5 {
+    public static String encryption(String code) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(code.getBytes());
+            byte b[] = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            return buf.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(encryption("xiaoxiaomo@com"));
+
+        System.out.println(String.format("%05d", 0));
+    }
+}

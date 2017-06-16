ce67bab65ca8462615c5ab6bbe2ace68580d2fcb
jasonTangxd <jason.tangxd@gmail.com>
2017-06-16 15:17:47 +0800

    flink table

--- flink/flink-table/src/main/java/com/xiaoxiaomo/flink/sql/WordCountSQL.java
+++ flink/flink-table/src/main/java/com/xiaoxiaomo/flink/sql/WordCountSQL.java
@@ -1,7 +1,62 @@
 package com.xiaoxiaomo.flink.sql;
 
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.java.BatchTableEnvironment;
+
 /**
- * Created by TangXD on 2017/6/16.
+ * Simple example that shows how the Batch SQL API is used in Java.
+ *
+ * <p>This example shows how to:
+ *  - Convert DataSets to Tables
+ *  - Register a Table under a name
+ *  - Run a SQL query on the registered Table
  */
 public class WordCountSQL {
+
+    public static void main(String[] args) throws Exception {
+
+        // set up execution environment
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        BatchTableEnvironment tEnv = TableEnvironment.getTableEnvironment(env);
+
+        DataSet<WC> input = env.fromElements(
+                new WC("Hello", 1),
+                new WC("Ciao", 1),
+                new WC("Hello", 1));
+
+        // register the DataSet as table "WordCount"
+        tEnv.registerDataSet("WordCount", input, "word, frequency");
+
+        // run a SQL query on the Table and retrieve the result as a new Table
+        Table table = tEnv.sql(
+                "SELECT word, SUM(frequency) as frequency FROM WordCount GROUP BY word");
+
+        DataSet<WC> result = tEnv.toDataSet(table, WC.class);
+
+        result.print();
+    }
+
+    // *************************************************************************
+    //     USER DATA TYPES
+    // *************************************************************************
+
+    public static class WC {
+        public String word;
+        public long frequency;
+
+        public WC() {}
+
+        public WC(String word, long frequency) {
+            this.word = word;
+            this.frequency = frequency;
+        }
+
+        @Override
+        public String toString() {
+            return "WC " + word + " " + frequency;
+        }
+    }
 }

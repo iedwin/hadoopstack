d1f2e0d1a8846626ffe7a4194c05a12ec2eb247a
jasonTangxd <jason.tangxd@gmail.com>
2017-06-16 14:55:51 +0800

    batch demo

---
+++ flink/flink-table/src/main/java/com/xiaoxiaomo/flink/sql/WordCountSQL.java
@@ -0,0 +1,7 @@
+package com.xiaoxiaomo.flink.sql;
+
+/**
+ * Created by TangXD on 2017/6/16.
+ */
+public class WordCountSQL {
+}
---
+++ flink/flink-table/src/main/java/com/xiaoxiaomo/flink/table/WordCountTable.java
@@ -0,0 +1,7 @@
+package com.xiaoxiaomo.flink.table;
+
+/**
+ * Created by TangXD on 2017/6/16.
+ */
+public class WordCountTable {
+}
